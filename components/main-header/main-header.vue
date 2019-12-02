47e403106c1bcaf3d1e254023c37048e139c44ce
PengYi <pengyi@zhihu.com>
2019-12-02 17:26:42 +0800

    feat: header 菜单

--- components/main-header/main-header.vue
+++ components/main-header/main-header.vue
@@ -7,7 +7,27 @@
         <!-- 搜索 -->
         <div id="search"></div>
         <!-- 个人信息 -->
-        <div id="avatar"></div>
+        <div id="avatar-menu" ref="menu" @click="showMenu">
+          <!-- 子菜单 -->
+          <div id="menu" v-show="menuVisible">
+            <!-- 用户信息 -->
+            <div id="avatar-container">
+              <div id="avatar-image"></div>
+              <div id="user-info">
+                <div id="user-info-name">用户昵称</div>
+                <div id="user-info-id">ID: 441·123·123</div>
+              </div>
+            </div>
+            <!-- 菜单 -->
+            <ul id="menu-list">
+              <li>我的主页</li>
+              <li>我的喜欢</li>
+              <li>隐私设置</li>
+            </ul>
+            <!-- 登出 -->
+            <div id="logout">登出账号</div>
+          </div>
+        </div>
         <!-- 操作 -->
         <div id="state"></div>
       </div>
@@ -27,6 +47,11 @@
 <script>
 export default {
   name: 'MainHeader',
+  data() {
+    return {
+      menuVisible: false
+    }
+  },
   computed: {
     isAgentPc() {
       return this.$store.getters.isAgentPc
@@ -34,6 +59,26 @@ export default {
   },
   created() {
     this.$style('main-header', true)
+  },
+  mounted() {
+    // 点击非菜单区域关闭菜单
+    document.addEventListener('click', (e) => {
+      if (!this.$refs.menu.contains(e.target)) {
+        this.hideMenu()
+      }
+    })
+  },
+  methods: {
+    showMenu(e) {
+      const target = e.target
+      const avatarMenu = document.getElementById('avatar-menu')
+      if (target === avatarMenu) {
+        this.menuVisible = !this.menuVisible
+      }
+    },
+    hideMenu() {
+      this.menuVisible = false
+    }
   }
 }
 </script>
